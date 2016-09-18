fe024f259dd160d5504d7fa312e8cc3f63752e0e
hong108 <hong108@aliyun.com>
2016-09-18 14:16:01 +0800

    20160919
    森

--- function_all.php
+++ function_all.php
@@ -2,12 +2,19 @@
 // this is code , function
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
+=======
+>>>>>>> parent of 65a03a5... ressss
 //不不不不不懂，英文英文英文
 
 function aaa(){
 		echo 'you open the door!';
+<<<<<<< HEAD
 	}
 =======
 >>>>>>> parent of 4af882f... 20160918-01
 =======
 >>>>>>> parent of 4af882f... 20160918-01
+=======
+	}
+>>>>>>> parent of 65a03a5... ressss
