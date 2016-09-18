8464cc672c601902c3cf17714cd9ef3d65686f6b
hong108 <hong108@aliyun.com>
2016-09-18 15:37:36 +0800

    del all

--- function_all.php
+++ function_all.php
@@ -1,23 +1,2 @@
 <?php
 // this is code , function
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-=======
->>>>>>> parent of 65a03a5... ressss
-//不不不不不懂，英文英文英文
-
-function aaa(){
-		echo 'you open the door!';
-<<<<<<< HEAD
-	}
-=======
->>>>>>> parent of 4af882f... 20160918-01
-=======
->>>>>>> parent of 4af882f... 20160918-01
-=======
-	}
->>>>>>> parent of 65a03a5... ressss
-=======
->>>>>>> parent of 58ed81c... Revert "function"
